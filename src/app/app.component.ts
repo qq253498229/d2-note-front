1ced054270ef627740a8d237f979b0c7496795b3
王斌 <253498229@qq.com>
2019-03-24 08:15:28 +0800

    polish weui cdn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles: [
+    '@import "https://res.wx.qq.com/open/libs/weui/1.1.3/weui.min.css";'
+  ],
 })
 export class AppComponent {
   title = 'd2-note-front';
