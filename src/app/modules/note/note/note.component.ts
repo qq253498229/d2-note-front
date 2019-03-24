086417c74d3d42b1e49e9562440604dc2ce9477c
王斌 <253498229@qq.com>
2019-03-25 01:39:55 +0800

    polish

--- src/app/modules/note/note/note.component.ts
+++ src/app/modules/note/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-note',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
+  list = [
+    {name: 'MF-SOR', complete: false},
+    {name: 'BH-PAL', complete: true},
+    {name: 'KC-AMA', complete: false},
+    {name: 'KB-NEC', complete: false}
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  add() {
+    this.list.push({name: 'TEST', complete: false});
+  }
 }
