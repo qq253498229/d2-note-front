5ad542468a9377e0ed89b5acf05a0e9a463ebb29
大胃王 <253498229@qq.com>
2019-03-25 15:55:16 +0800

    polish-笔记详情

--- src/app/modules/note/detail/detail.component.ts
+++ src/app/modules/note/detail/detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  types = [
+    {code: 1, name: '武器'},
+    {code: 2, name: '副手'},
+    {code: 3, name: '头盔'},
+    {code: 4, name: '衣服'},
+    {code: 5, name: '腰带'},
+    {code: 7, name: '手套'},
+    {code: 6, name: '鞋'},
+    {code: 7, name: '项链'},
+    {code: 8, name: '戒指'},
+    {code: 9, name: '护身符'},
+    {code: 10, name: '附文'}
+  ];
+
+  accounts = [
+    {code: 1, name: 'mf-sor'},
+    {code: 2, name: 'kc-ama'},
+    {code: 3, name: 'bo-bar'},
+    {code: 4, name: 'kb-nec'},
+    {code: 5, name: 'bh-pal'}
+  ];
+
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  save() {
+    console.log(1);
+  }
 }
